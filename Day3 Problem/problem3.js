0aaa8a2427670774c3556e8fc48cbb69e036c783
akshaychavan010101 <akshaychavan010101@gmail.com>
2022-10-11 00:08:34 +0530

    Day3 Problem uploaded

---
+++ Day3 Problem/problem3.js
@@ -0,0 +1,13 @@
+// Problem 3: Given and character if it is a consonant print "Consonant"
+
+// let character = "b";
+// if (character == "a" && character == "e" || character == "i" || character == "o" || character == "u"){
+//   console.log("");
+// }else {
+//   console.log("Consonant");
+// }
+
+
+let character = "a";
+character == "a" || character == "e" || character == "i" || character == "o" || character == "u" ? console.log("") : console.log("Consonant");
+
---
+++ Day3 Problem/problem4.js
@@ -0,0 +1,17 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+// let a = 10;
+// let b = 25;
+// let c = 30;
+// if (a>b && a>c){
+//   console.log(a,"is greater ");
+// }else if (b>a && b>c){
+//   console.log(b,"is greater");
+// }else if (c>b && c>a){
+//   console.log(c,"is greater");
+// }
+
+
+let a = 40;
+let b = 25;
+let c = 30;
+a>b && a>c ? console.log(a,"is greater"): b>a && b>c ? console.log(b,"is greater") : c>a && c>b ? console.log(c,"is greater"): console.log("");
